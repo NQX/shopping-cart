20b7fff5b4f91cd69357cfbf07d0572225f2526c
NQX <nqxdev@gmail.com>
2020-12-26 15:18:31 +0100

    added frontend (reactjs)

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv').config({path: './.ENV'});
+const path = require('path');
+require('dotenv').config({path: './.ENV'});
 
 const app = express();
 
 
 app.use('/files', express.static('files'));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
